57dc9d5f40ee207259ccfa83f055a04c0ac6c862
yaoxr <suanrong@gmail.com>
2018-01-15 11:31:25 +0800

    Modify: config

--- main.py
+++ main.py
@@ -21,16 +21,22 @@ from utils.utils import *
 import scipy.io as sio
 import time
 import copy
-
+from optparse import OptionParser
 
 if __name__ == "__main__":
-    config = Config()
+    parser = OptionParser()
+    parser.add_option("-c",dest = "config_file", action = "store", metavar = "CONFIG FILE")
+    options, _ = parser.parse_args()
+    if options.config_file == None:
+        options.config_file = 'config.ini'
+
+    config = Config(options.config_file)
     
-    origin_graph_data = Graph(config.origin_file_path, config.ng_sample_ratio)
-    train_graph_data = Graph(config.train_file_path, config.ng_sample_ratio)
+    origin_graph_data = Graph(config.origin_graph_file, config.ng_sample_ratio)
+    train_graph_data = Graph(config.train_graph_file, config.ng_sample_ratio)
     
     #load label for classification
-    #graph_data.load_label_data(config.label_file_path)
+    #graph_data.load_label_data(config.label_file)
     
     config.struct[0] = train_graph_data.N
     
@@ -41,17 +47,17 @@ if __name__ == "__main__":
     batch_n = 0
     
     #graph_data = graph_data.subgraph(config.sample_method, config.sample_ratio)
-    
-    fout = open(config.embedding_filename + "-log.txt","w") 
+    tt = time.ctime()
+    fout = open(config.embedding_filename + '-' + tt +  "-log.txt","w") 
     while (True):
         mini_batch = train_graph_data.sample(config.batch_size)
         loss = model.fit(mini_batch)
         batch_n += 1
-        print "Epoch : %d, batch : %d, loss: %.3f" % (epochs, batch_n, loss)
+        #print "Epoch : %d, batch : %d, loss: %.3f" % (epochs, batch_n, loss)
         if train_graph_data.is_epoch_end:
             epochs += 1
             batch_n = 0
-            print "Epoch : %d loss : %.3f" % (epochs, loss)
+            loss = 0
             if epochs % config.display == 0:
                 embedding = None
                 while (True):
@@ -65,14 +71,15 @@ if __name__ == "__main__":
                     if train_graph_data.is_epoch_end:
                         break
 
-                result = check_link_prediction(embedding, train_graph_data, origin_graph_data, [100,200,300,400,500,600,700,800,900,1000])
+                print "Epoch : %d loss : %.3f" % (epochs, loss)
+                #check_link_reconstruction(embedding, train_graph_data, np.arange(100,1000,100))
+                result = check_link_prediction(embedding, train_graph_data, origin_graph_data, [10, 100, 500, 1000, 10000])
                 #data = origin_data.sample(origin_data.N, with_label = True)
                 #check_multi_label_classification(model.get_embedding(data), data.label)
                 print >> fout, epochs, result
             if epochs > config.epochs_limit:
                 print "exceed epochs limit terminating"
                 break
-            last_loss = loss
-    embedding = model.get_embedding(origin_graph_data.sample(origin_graph_data.N, do_shuffle = False))
-    sio.savemat(config.embedding_filename + '-' + '_embedding.mat',{'embedding':embedding})
+    embedding = model.get_embedding(train_graph_data.sample(origin_graph_data.N, do_shuffle = False))
+    sio.savemat(config.embedding_filename + '-' + tt + '_embedding.mat',{'embedding':embedding})
     fout.close()
